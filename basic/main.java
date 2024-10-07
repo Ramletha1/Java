78600819a3f48321265b5938d2e5628037cdba26
Ramletha1 <146055491+Ramletha1@users.noreply.github.com>
2024-10-07 04:41:57 +0000

    /home/codespace

---
+++ basic/main.java
@@ -0,0 +1,5 @@
+public static main(){
+    int x,y,inp;
+    System.in.print(x);
+    System.out.printf("Input number is %d.",x);
+}
--- main.java
+++ main.java
@@ -1,16 +1,14 @@
 import java.util.*;
-class main{
 
-    public class main{
-        public static void main(String[] args){
-        System.out.println("Hello World");
-        int x; double y; String s;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter integer = ");
-        System.out.print("Enter float = ");
-        System.out.print("Enter string = ");
-        System.out.printf("x=%d, y=%.2f, s=%s\n",x,y,s);
-        System.out.println("Bye!");
-        }
+public class main{
+    public static void main(String[] args){
+    System.out.println("Hello World");
+    int x; double y; String s;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter integer = ");
+    System.out.print("Enter float = ");
+    System.out.print("Enter string = ");
+    System.out.printf("x=%d, y=%.2f, s=%s\n",x,y,s);
+    System.out.println("Bye!");
     }
 }
