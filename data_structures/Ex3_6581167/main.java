f636189c468198938fdbf5b0c6ceaa018aa2cddc
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2024-10-10 03:14:37 +0000

    .

--- data_structures/Ex3_6581167/main.java
+++ data_structures/Ex3_6581167/main.java
@@ -116,8 +116,8 @@ public class main {
         String userInput;
 
         try {
-            //Scanner fileScanner = new Scanner(new File("/workspaces/Java/data_structures/Ex3_6581167/languages.txt"));
-            Scanner fileScanner = new Scanner(new File("src/main/java/Ex3_6581167/languages.txt"));
+            Scanner fileScanner = new Scanner(new File("/workspaces/Java/data_structures/Ex3_6581167/languages.txt"));
+            //Scanner fileScanner = new Scanner(new File("src/main/java/Ex3_6581167/languages.txt"));
             fileScanner.nextLine();
             while (fileScanner.hasNextLine()){
                 String line = fileScanner.nextLine();
