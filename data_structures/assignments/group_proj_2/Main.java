167c92e713f649058a1b6d1b1577e7d0ccabdbc1
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2024-11-25 17:17:09 +0000

    /home/codespace

--- data_structures/assignments/group_proj_2/Main.java
+++ data_structures/assignments/group_proj_2/Main.java
@@ -19,10 +19,12 @@ public class Main {
         scanner.close();
     }
 
+
     public static boolean askInput(Scanner scanner) {
         int N, numInput;
-        String strInput;
+        String[] strInput;
 
+        // Ask 1
         System.out.println("Enter N for N*N board (N must be at least 5)");
         while (true) {
             try {
@@ -35,41 +37,97 @@ public class Main {
             } catch (NumberFormatException e) { System.out.println("Invalid input."); }
         }
 
-        Board board = new Board(N);
-        board.printBoardID();
-
+        // Ask 2
         System.out.println("Enter Knight ID");
         while (true) {
             try {
                 numInput = Integer.parseInt(scanner.nextLine());
-                if (numInput < 0 || numInput > N*N) {
+                if (numInput < 0 || numInput > (N*N)-1) {
                     System.out.println("Input must be according to Cell IDs.");
                     continue;
                 }
                 break;
             } catch (NumberFormatException e) { System.out.println("Invalid input."); }
         }
+        int knightPos = numInput;
 
+        // Ask 3
+        System.out.println("Enter Castle ID");
+        while (true) {
+            try {
+                numInput = Integer.parseInt(scanner.nextLine());
+                if (numInput < 0 || numInput > (N*N)-1) {
+                    System.out.println("Input must be according to Cell IDs.");
+                    continue;
+                }
+                break;
+            } catch (NumberFormatException e) { System.out.println("Invalid input."); }
+        }
+        int castlePos = numInput;
+
+        // Ask 4
+        System.out.println("Enter bomb IDs separated by comma (Invalid IDs will be ignored)");
+        HashSet<Integer> bombCell = new HashSet<>();
+        strInput = scanner.nextLine().split(",");
+        for (String bomb : strInput) {
+            try {
+                bombCell.add(Integer.parseInt(bomb.trim()));
+            } catch (NumberFormatException e) { /* Ignore Invalid Input */ }
+        }
+
+
+        Map<String, Cell> board = new HashMap<>();
+        for (int row=0; row<N; row++) {     // Initialize
+            for (int col=0; col<N; col++) {
+                String position = row + "," + col;
+                board.put(position, new Cell(row, col, ' '));   // Empty Cell
+            }
+        }
+        
+        for (int bomb : bombCell) {
+            int bombRow = bomb/N;
+            int bombCol = bomb%N;
+            // System.out.printf("\n\nBombCell:%d row:%d col:%d", bomb, bombRow, bombCol); // Check
+        }
         return true;
     }
-}
 
-class Board {
-    private int N;
-
-    public Board(int N) {
-        this.N = N;
-    }
-
-    public void printBoardID() {
-        for (int i=0; i<N; i++) {
-            if (i == 0) System.out.printf("%8s", "Cell IDs");
-            else System.out.printf("%8s", " ");
-
-            for (int j=0; j<N; j++) {
-                System.out.printf("%5d", j+(i*N));
-            }
-            System.out.println();
-        }
+    public static boolean setCell(Map<String, Cell> board, int row, int col, char type) {
+        String position = row + "," + col;
+        if (board.get(position).type == ' ') {
+            board.get(position).type = type;
+            return true;
+        } else return false;
     }
 }
+
+
+class Cell { // Cells
+    int row, col;
+    char type;
+
+    public Cell(int row, int col, char type) {
+        this.row = row;
+        this.col = col;
+        this.type = type;
+    }
+    
+    @Override
+    public String toString() {
+        return type + "";
+    }
+}
+
+
+
+/*
+for (int i=0; i<N; i++) {
+    if (i == 0) System.out.printf("%8s", "Cell IDs");
+    else System.out.printf("%8s", " ");
+
+    for (int j=0; j<N; j++) {
+        System.out.printf("%5d", j+(i*N));
+    }
+    System.out.println();
+}
+*/
