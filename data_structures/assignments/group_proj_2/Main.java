780cb2317cc95c24bcca20961f510f6e601a8207
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2024-11-25 14:46:34 +0000

    /home/codespace

--- data_structures/assignments/group_proj_2/Main.java
+++ data_structures/assignments/group_proj_2/Main.java
@@ -1,16 +1,37 @@
 // package EGCI221-Project1;
 import java.util.*;
 
-public Main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
-            if (!askInput) {
+            if (!askInput(scanner)) {
                 System.out.println("Exiting the program...");
                 break;
             }
         }
         scanner.close();
+    }
+
+    public static boolean askInput(Scanner scanner) {
+        String[][] board
+    }
+}
+
+class BoardPlacement {
+    private int row, col, moves;
+    private String[][] board;
+
+    private static final int[][] knightMoves = {{ 2,-1},{ 2, 1},    // Up
+                                                {-2,-1},{-2, 1},    // Down
+                                                { 1,-2},{-1,-2},    // Left
+                                                { 1, 2},{-1, 2}};   // Right
+
+    public Board(int row, int col, int moves) {
+        this.row = row;
+        this.col = col;
+        this.moves = moves;
+        this.board = new board[row][col];
     }
 }
