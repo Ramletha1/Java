fa01146435e5e397890fbdbef170982577ca4816
Wongsatorn Suwannarit <blue25477@gmail.com>
2024-12-05 20:14:02 +0700

    ~

--- data_structures/assignments/group_proj_2/Main.java
+++ data_structures/assignments/group_proj_2/Main.java
@@ -31,15 +31,14 @@ public class Main {
 // ==============================================================================================================================
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<Integer, Cell> board = new HashMap<>();
+        HashMap<Integer, Cell> board = new HashMap<>();
         
         while (true) {
             askInput(board, scanner); 
-            printBoard(board);
             BFS(board);
 
             System.out.println("New game? (y - continue, other - exit)");
-            if (scanner.nextLine().contains("y")) continue;
+            if (scanner.nextLine().toLowerCase().contains("y")) continue;
             else {
                 System.out.println("Exiting the program...");
                 break;
@@ -48,7 +47,7 @@ public class Main {
         scanner.close();
     }
 // ==============================================================================================================================
-    public static void askInput(Map<Integer, Cell> board, Scanner scanner) {
+    public static void askInput(HashMap<Integer, Cell> board, Scanner scanner) {
         int numInput;
         String[] strInput;
 
@@ -114,18 +113,18 @@ public class Main {
         strInput = scanner.nextLine().split(",");
         for (String bombInput : strInput) {
             try {
-                int bombID = Integer.parseInt(bombInput);
+                int bombID = Integer.parseInt(bombInput.trim());
                 if (bombID < 0 || bombID > (N*N)-1) continue;   // Ignore Invalid number input
                 setCell(board, bombID, 'b');    // Add BOMB to Board
             } catch (NumberFormatException e) { /* Ignore Error Input */ }
         }
     }
 // ==============================================================================================================================
-    public static void setCell(Map<Integer, Cell> board, int cellID, char type) {
+    public static void setCell(HashMap<Integer, Cell> board, int cellID, char type) {
         if (board.get(cellID).type == ' ') board.get(cellID).type = type;
     }
 // ==============================================================================================================================
-    public static void printBoard(Map<Integer, Cell> board) {   // Printing at start
+    public static void printBoard(HashMap<Integer, Cell> board) {   // Printing at start
         System.out.printf("%8s", "Cell IDs");
         for (int cellID=0; cellID<N*N; cellID++) {
             System.out.printf("%5d: %2s", cellID, board.get(cellID).type);
@@ -136,58 +135,80 @@ public class Main {
 // ==============================================================================================================================
     public static void printPath(GraphPath<Integer, DefaultEdge> path, HashSet<Integer> bombCell) {
         int moveCount = 0;
-        for (int move : path.getVertexList()) {
+        ArrayList<Integer> moveOrders = new ArrayList<>();
+        for (int knightPath : path.getVertexList()) {
             if (moveCount == 0) System.out.printf("Initially, Knight at [%d]\n", knightPos, "");
-            else System.out.printf("Move %d --> Jump to [%d]\n", moveCount, move, "");
+            else {
+                System.out.printf("Move %d --> Jump to [%d]\n", moveCount, knightPath, "");
+                moveOrders.add(knightPath);  // From path
+            }
             for (int cellID=0; cellID<N*N; cellID++) {
-                System.out.printf("%5d: ", cellID);
+                System.out.printf("%4d: ", cellID);
                 String type =   (bombCell.contains(cellID)) ? "b"
-                              : (cellID == move) ? "K*"
+                              : (cellID == knightPath) ? "K*"
                               : (cellID == castlePos) ? "C*"
                               : "";
-                System.out.printf("%2s", type);
-                if (cellID%N == N-1) System.out.printf("\n", "");
+                if (moveCount == path.getLength() && cellID == castlePos) type = "C+K";
+                System.out.printf("%-3s", type);
+                if (cellID%N == N-1) System.out.println();
             }
-            if (moveCount++ == 0) System.out.printf("\nBest route to Castle = %d moves.\n", path.getLength());
+            moveCount++;
+            //if (moveCount++ == 0) System.out.printf("\nBest route to Castle = %d moves.\n", path.getLength());
             System.out.println();
         }
+
+        System.out.printf("Move = [%d", knightPos);
+        for (int moveOrder : moveOrders) {
+            System.out.printf(" > %d", moveOrder);
+        }
+        System.out.printf("]\nBest route to Castle = %d moves.\n", path.getLength());
     }
 // ==============================================================================================================================
-    public static void BFS(Map<Integer, Cell> board) {  // *** BREADTH-FIRST SEARCH ***
+    public static void BFS(HashMap<Integer, Cell> board) {  // *** BREADTH-FIRST SEARCH ***
         Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
         HashSet<Integer> bombCell = new HashSet<>();    // To store Bomb cellID
 
-        for (Map.Entry<Integer, Cell> entry : board.entrySet()) {
+        for (HashMap.Entry<Integer, Cell> entry : board.entrySet()) {
             if (entry.getValue().type != 'b') {     // Add valid CellID as vertex
-                graph.addVertex(entry.getKey());
+                int cellID = entry.getKey();
+                graph.addVertex(cellID);
                 // System.out.println("Adding [" + entry.getKey() + "]: " + entry.getValue());
             }
             else bombCell.add(entry.getKey());      // Store bomb data for later use
         }
 
         for (Integer cellID : graph.vertexSet()) {  // Add valid moves as edges
+            HashMap<Integer, Boolean> visited = new HashMap<>();   // Storing Visited Cell for faster search.............................
             Cell cell = board.get(cellID);
             for (int[] move : knightMoves) {    // Check KnightMove rule
                 int nextRow = cell.row + move[0];
                 int nextCol = cell.col + move[1];
 
-                if (isValidMove(board, nextRow, nextCol)) { // Function check for boundary and Bomb
+                if (isValidMove(board, visited, nextRow, nextCol)) { // Function check for boundary and Bomb
                     int nextCell = (nextRow * N) + nextCol;
                     graph.addEdge(cellID, nextCell);
                 }
             }
+            visited.put(cellID, true);
         }
         BFSShortestPath<Integer, DefaultEdge> bfs = new BFSShortestPath<>(graph);   // Use build-in algorithm
         GraphPath<Integer, DefaultEdge> path = bfs.getPath(knightPos, castlePos);   // Finding all possible path bfs.getPath(SOURCE, TARGET);
 
-        if (path == null) System.out.println("No path found.");
+        if (path == null) System.out.println("\nNo path found.");
         else printPath(path, bombCell);
+
+        for (int i=0; i<N; i++) {
+            System.out.printf("=======");
+        }
+        System.out.println();
     }
 // ==============================================================================================================================
-    public static boolean isValidMove(Map<Integer, Cell> board, int row, int col) {
+    public static boolean isValidMove(HashMap<Integer, Cell> board, HashMap<Integer,Boolean> visited, int row, int col) {
         if (row < 0 || row >= N || col < 0 || col >= N) return false;   // Check boundary
+
         int cellID = (row * N) + col;
-        return board.get(cellID).type != 'b';   // B O M B ! ! !
+        if (board.get(cellID).type != 'b' && visited.containsKey(cellID) == false) return true; // Check for no bomb and not visited
+        return false;
     }
 // ==============================================================================================================================
 }
