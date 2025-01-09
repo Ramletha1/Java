3b5ff77ceb85bab47756cf4fdf58aab3b046c62e
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2025-01-09 08:38:35 +0000

    /home/codespace

---
+++ paradigms/LAB/Week_1_2/Main.java
@@ -0,0 +1,12 @@
+// Wongastorn Suwannarit 6581167
+
+import javax.swing.*;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+
+        String name = "Hello ";
+        System.out.println(name.repeat(5));
+    }
+}
