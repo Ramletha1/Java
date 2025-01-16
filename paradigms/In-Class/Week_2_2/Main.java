60148fc8487ce6420447685897d47ef53fb4c5f3
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2025-01-16 08:37:10 +0000

    /home/codespace

--- paradigms/In-Class/Week_2_2/Main.java
+++ paradigms/In-Class/Week_2_2/Main.java
@@ -5,5 +5,15 @@ public class Main {
 
         System.out.println("Hello World!");
 
+        int[][] array1 = new int[3][3];
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.printf("{ ");
+            for (int j = 0; j < array1[i].length; j++) {
+                array1[i][j] = (i+1)*(j+1);
+                System.out.printf("%-2d ", array1[i][j]);
+            }
+            System.out.println("}");
+        }
     }
 }
