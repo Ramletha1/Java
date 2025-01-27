e1323590a41e3a0fe21fb961849b0cf15af99618
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2025-01-27 21:27:22 +0000

    /home/codespace

--- paradigms/Assignments/Ex3_6581167/Main.java
+++ paradigms/Assignments/Ex3_6581167/Main.java
@@ -32,8 +32,20 @@ class Player {
 }
 
 class FootballPlayer extends Player {
-    public FootballPlayer(String nm, int by) {
+    protected int[] arrayOfGames;
+    protected int[] arrayOfGoals;
+    protected float avgGoals;
+
+    protected int totalGames = 0;
+    protected int totalGoals = 0;
+
+    public FootballPlayer(String nm, int by, int[] tga, int[] tgo) {
         super(nm, by);
+        this.arrayOfGames = tga;
+        this.arrayOfGoals = tgo;
+        for (int game : arrayOfGames) this.totalGames += game;
+        for (int goal : arrayOfGoals) this.totalGoals += goal;
+        this.avgGoals = (float)totalGoals/totalGames;
     }
 
     @Override
@@ -43,13 +55,24 @@ class FootballPlayer extends Player {
 
     @Override
     public void printStat() {
-.
+        System.out.printf("%-25s total games = %-6d total goals = %-3d (%.2f per game)      last season goals = %2d\n", getName(), totalGames, totalGoals, avgGoals, arrayOfGoals[arrayOfGoals.length-1]);
     }
 }
 
 class BasketballPlayer extends Player {
-    public BasketballPlayer(String nm, int by) {
+    protected int totalGames;
+    protected int totalMins;
+    protected int totalPts;
+    protected float avgMins;
+    protected float avgPts;
+
+    public BasketballPlayer(String nm, int by, int tga, int tmi, int tpt) {
         super(nm, by);
+        this.totalGames = tga;
+        this.totalMins = tmi;
+        this.totalPts = tpt;
+        this.avgMins = (float)totalMins/totalGames;
+        this.avgPts = (float)totalPts/totalGames;
     }
 
     @Override
@@ -59,27 +82,42 @@ class BasketballPlayer extends Player {
 
     @Override
     public void printStat() {
-        
+        System.out.printf("%-25s total games = %-6d total mins = %-4d (%.2f per game)     total points = %-5d (%.2f per game)\n", getName(), totalGames, totalMins, avgMins, totalPts, avgPts);
     }
 }
 
 public class Main {
     public static void main (String[] args) {
         File inFile = new File("players.txt");
-        Player[] players = new Player[Constant.TOTAL_PLAYER];
+        Player[] allPlayers = new Player[Constant.TOTAL_PLAYER];
 
         try {
             Scanner readFile = new Scanner(inFile);
-            for (int i=0; i<players.length; i++) {
+            for (int i = 0; i<allPlayers.length; i++) {
                 String line = readFile.nextLine();
                 String[] col = line.trim().split("\\s*,\\s*");
+
+                String name = col[1];
+                int birthyear = Integer.parseInt(col[2]);
                 
                 if (col[0].equalsIgnoreCase("B")) {
-                    //System.out.printf("\nBasketBall");
-                    players[i] = new BasketballPlayer(col[1],Integer.parseInt(col[2]));
+                    // System.out.printf("\nBasketBall");
+                    int totalGames = Integer.parseInt(col[3]);
+                    int totalMins = Integer.parseInt(col[4]);
+                    int totalPts = Integer.parseInt(col[5]);
+
+                    allPlayers[i] = new BasketballPlayer(name, birthyear, totalGames, totalMins, totalPts);
+
                 } else if (col[0].equalsIgnoreCase("F")) {
-                    //System.out.printf("\nFootBall  ");
-                    players[i] = new FootballPlayer(col[1],Integer.parseInt(col[2]));
+                    // System.out.printf("\nFootBall  ");
+                    int[] totalGames = new int[col.length - 3];
+                    int[] totalGoals = new int[col.length - 3];
+                    for (int j = 3; j<col.length; j++) {
+                        String[] subCol = col[j].split(":");
+                        totalGames[j-3] = Integer.parseInt(subCol[0]);
+                        totalGoals[j-3] = Integer.parseInt(subCol[1]);
+                    }
+                    allPlayers[i] = new FootballPlayer(name, birthyear, totalGames, totalGoals);
                 }
             }
             readFile.close();
@@ -87,10 +125,26 @@ public class Main {
 
 
         System.out.println("=== All Player data (by reverse order) ===");
-        for (int i = players.length-1; i>=0; i--) {
-            players[i].printPersonalData();
-        }
+        for (int i = allPlayers.length-1; i>=0; i--) {
+            allPlayers[i].printPersonalData();
+        } System.out.println();
 
-        System.out.println("=== Football player statistics (by input order)");
+
+        System.out.println("=== Football player statistics (by input order) ===");
+        for (int i = 0; i<allPlayers.length; i++) {
+            if (allPlayers[i] instanceof FootballPlayer) {
+                FootballPlayer player = (FootballPlayer) allPlayers[i];
+                player.printStat();
+            }
+        } System.out.println();
+
+
+        System.out.println("=== Basketball player statistics (by input order) ===");
+        for (int i = 0; i<allPlayers.length; i++) {
+            if (allPlayers[i] instanceof BasketballPlayer) {
+                BasketballPlayer player = (BasketballPlayer) allPlayers[i];
+                player.printStat();
+            }
+        } System.out.println();
     }
 }
